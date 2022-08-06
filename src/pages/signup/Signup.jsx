7e111664e46a15c6dffeb1df29e4cc8b7f15e2aa
Matuko <matuko@Dani-Matuko.lan>
2022-08-06 12:03:13 +0300

    add signup form

--- src/pages/signup/Signup.jsx
+++ src/pages/signup/Signup.jsx
@@ -1,8 +1,48 @@
-import React from "react";
-import styles from "./signup.module.css";
+import React, { useState } from 'react';
+import styles from './signup.module.css';
 
 const Signup = () => {
-  return <div>Signup</div>;
+  const [displayName, setDisplayName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(displayName, email, password);
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className={styles['signup-form']}>
+      <h1>Signup</h1>
+      <label>
+        <span>Display Name:</span>
+        <input
+          type="text"
+          value={displayName}
+          onChange={(e) => setDisplayName(e.target.value)}
+        />
+      </label>
+      <label>
+        <span>Email:</span>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </label>
+      <label>
+        <span>Password:</span>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </label>
+      <button type="submit" className="btn">
+        Signup
+      </button>
+    </form>
+  );
 };
 
 export default Signup;
