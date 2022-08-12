3b1e31589cd942520e634cd3db22d100f59abed1
Matuko <matuko@Dani-Matuko.lan>
2022-08-12 21:26:13 +0300

    show menu depending on user type

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,25 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './Navbar.module.css';
 import { Link } from 'react-router-dom';
 import { useLogout } from '../../hooks/useLogout';
+import { useAuthContext } from '../../hooks/useAuthContext';
 
 const Navbar = () => {
   const { logout } = useLogout();
+  const { user } = useAuthContext();
+  const [loggedInMenu, setLoggedInMenu] = useState(false);
+
   return (
     <nav className={styles.navbar}>
       <ul>
         <li className={styles.title}>myMoney </li>
-        <li>
-          <Link to="/signup">Signup</Link>
-        </li>
-        <li>
-          <Link to="/login">Login</Link>
-        </li>
-        <li>
-          <button className="btn" onClick={logout}>
-            Logout
-          </button>
-        </li>
+        {!user ? (
+          <>
+            <li>
+              <Link to="/signup">Signup</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+          </>
+        ) : (
+          <>
+            <li>
+              {user.displayName}
+              <button className="btn" onClick={logout}>
+                Logout
+              </button>
+            </li>
+          </>
+        )}
       </ul>
     </nav>
   );
